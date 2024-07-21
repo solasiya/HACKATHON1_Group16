7b2d1817e9bb0647b734bbfcfdcf615f2b276919
Solomon Asiya <solasiya@gmail.com>
2024-07-22 01:30:54 +0200

    Home Nav Update

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const articles = document.querySelectorAll('article');
-  
-    articles.forEach(article => {
-      article.addEventListener('mouseenter', () => {
-        article.style.backgroundColor = '#f0f0f0';
-        article.style.transform = 'scale(1.05)';
-        article.style.transition = 'all 0.3s';
-      });
-  
-      article.addEventListener('mouseleave', () => {
-        article.style.backgroundColor = 'transparent';
-        article.style.transform = 'scale(1)';
-      });
-    });
-  });
-  
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
